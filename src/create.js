e5d08b6dc9ebc484855e965a6703e8bc88a0d0d5
José Francisco Vargas Galán <48479726+josevargasgalan@users.noreply.github.com>
2019-05-29 11:08:01 +0200

    Lesson Done

--- src/create.js
+++ src/create.js
@@ -1,9 +1,16 @@
 import { displayLog } from './utils';
+import { fromEvent } from 'rxjs'
 
 export default () => {
     /** start coding */
+    const actionBtn = document.getElementById('action-btn');
+    const source = fromEvent(actionBtn, 'click');
 
-
-
+    source.subscribe(event => {
+        displayLog(`Click event at position ${event.x}, ${event.y}`);
+    })
+    fromEvent(document, 'mousemove').subscribe(event => {
+        console.log(event);
+    });
     /** end coding */
 }
