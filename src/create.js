b713d931c238a661da5251884b9c21b549abc611
José Francisco Vargas Galán <48479726+josevargasgalan@users.noreply.github.com>
2019-05-29 10:45:23 +0200

    Lesson Done

--- src/create.js
+++ src/create.js
@@ -1,9 +1,17 @@
 import { displayLog } from './utils';
+import { of, range } from 'rxjs'
 
 export default () => {
     /** start coding */
 
-
-
+    const source = of(1,2,3,4,5,6);
+    const source2 = of([1,2,3], "Hello World", {foo: "bar"}, function sayHello() {
+        return "Hi!";
+    });
+    //Rango de numeros que empieza en 2 y tiene 5 valores
+    const source3 = range(2,5);
+    const subscription = source3.subscribe((event) => {
+        displayLog(event);
+    })
     /** end coding */
 }
