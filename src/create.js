51ee493da6032ae868f6bdd0fe5ac917fafec665
José Francisco Vargas Galán <48479726+josevargasgalan@users.noreply.github.com>
2019-05-29 10:03:25 +0200

    Lesson done

--- src/create.js
+++ src/create.js
@@ -1,9 +1,19 @@
-import { displayLog } from './utils';
+import { displayLog } from "./utils";
+import { Observable } from "rxjs";
 
 export default () => {
-    /** start coding */
+  /** start coding */
 
+  const hello = Observable.create(observer => {
+    observer.next("Hello");
+    setTimeout(() => {
+      observer.next("world");
+    }, 2000);
+  });
 
+  const subscribe = hello.subscribe(event => {
+    displayLog(event);
+  });
 
-    /** end coding */
-}
+  /** end coding */
+};
