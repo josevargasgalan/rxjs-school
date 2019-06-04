0c12f81a8b8806591bb710fbe802305e54395a0e
José Francisco Vargas Galán <48479726+josevargasgalan@users.noreply.github.com>
2019-06-04 08:40:30 +0200

    Lesson done

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,9 +1,24 @@
-import { displayLog } from './utils';
-
+import { displayLog } from "./utils";
+import { fromEvent } from "rxjs";
+import { mapTo, map, filter } from "rxjs/operators";
 export default () => {
-    /** start coding */
-    const grid = document.getElementById('grid');
+  /** start coding */
+  const grid = document.getElementById("grid");
+  /** MapTo */
+  //   const click$ = fromEvent(grid, 'click').pipe(
+  //       mapTo('CLICK')
+  //   );
 
+  /** Map */
+  //   const click$ = fromEvent(grid, "click").pipe(
+  //     map(val => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)])
+  //   );
 
-    /** end coding */
-}
+  /** Filter */
+  const click$ = fromEvent(grid, "click").pipe(
+    map(val => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+    filter(val => (val[0] + val[1]) % 2 != 0)
+  );
+  const subscribe = click$.subscribe(data => displayLog(data));
+  /** end coding */
+};
