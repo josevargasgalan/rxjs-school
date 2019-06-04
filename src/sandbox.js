32c04b2d5472583b4fec8964ae7b5e520fcdf284
José Francisco Vargas Galán <48479726+josevargasgalan@users.noreply.github.com>
2019-06-04 09:01:36 +0200

    Lesson done

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,19 +1,31 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, takeWhile } from 'rxjs/operators';
-
+import { map, takeWhile, last, takeLast, skip, tap } from 'rxjs/operators';
 export default () => {
-    /** start coding */
-    const grid = document.getElementById('grid');
-    const click$ = fromEvent(grid, 'click').pipe(
-        map(val => [ 
-            Math.floor(val.offsetX/50), 
-            Math.floor(val.offsetY/50)
-        ]),
-        takeWhile( ([col, row]) => col > 3 )
-    );
+  /** start coding */
+  const grid = document.getElementById('grid');
+  /** Last */
+  //   const click$ = fromEvent(grid, 'click').pipe(
+  //     map(val => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+  //     takeWhile(val => val[0] > 3),
+  //     tap(val => console.log(`Valid in takeWhile ${val}`)),
+  //     last()
+  //   );
 
-    const subscription = click$.subscribe(data => displayLog(data));
+  /** TakeLast */
+//   const click$ = fromEvent(grid, 'click').pipe(
+//     map(val => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+//     takeWhile(val => val[0] > 3),
+//     tap(val => console.log(`Valid in takeWhile ${val}`)),
+//     takeLast(3)
+//   );
 
-    /** end coding */
-}
+  /** Skip */
+  const click$ = fromEvent(grid, 'click').pipe(
+    map(val => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+    tap(val => console.log(`cell: ${val}`)),
+    skip(5)
+  );
+  const subscribe = click$.subscribe(data => displayLog(data));
+  /** end coding */
+};
