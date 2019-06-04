8de62bc91dd28cad3affe000a36421f9d60cb425
José Francisco Vargas Galán <48479726+josevargasgalan@users.noreply.github.com>
2019-06-04 09:41:55 +0200

    Lesson done

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,22 +1,31 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, takeWhile, tap } from 'rxjs/operators';
-
+import {
+  map,
+  takeWhile,
+  tap,
+  distinct,
+  distinctUntilChanged
+} from 'rxjs/operators';
 export default () => {
-    /** start coding */
-    const grid = document.getElementById('grid');
-    const click$ = fromEvent(grid, 'click').pipe(
-        map(val => [ 
-            Math.floor(val.offsetX/50), 
-            Math.floor(val.offsetY/50)
-        ]),
-        takeWhile( ([col, row]) => col != 0 ),
-        tap(val => console.log(`cell: [${val}]`)),
-        map(([col, row]) => col+row),
-        tap(val => console.log('sum of col + row is:', val)),
-    );
+  /** start coding */
+  const grid = document.getElementById('grid');
+  /** Distinct - no deja pasar ningun valor que coincida con alguno ya emitido en el pasado*/
+  //   const click$ = fromEvent(grid, 'click').pipe(
+  //     map(val => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+  //     takeWhile(val => val[0] !== 0),
+  //     tap(val => console.log(`cell: ${val}`)),
+  //     distinct(val => `${val[0]} - ${val[1]}`)
+  //   );
 
-    const subscription = click$.subscribe(data => displayLog(data));
-
-    /** end coding */
-}
+  /** DistinctUntilChanged */
+  const click$ = fromEvent(grid, 'click').pipe(
+    map(val => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+    takeWhile(val => val[0] !== 0),
+    tap(val => console.log(`cell: ${val}`)),
+    distinctUntilChanged((cell1, cell2) => 
+      (cell1[0] === cell2[0]) && (cell1[1] === cell2[1]))
+  );
+  const subscribe = click$.subscribe(data => displayLog(data));
+  /** end coding */
+};
