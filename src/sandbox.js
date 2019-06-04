458c66d039eae4cd6db4ca0928f104393a7e3d98
José Francisco Vargas Galán <48479726+josevargasgalan@users.noreply.github.com>
2019-06-04 08:44:56 +0200

    Lesson done

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,18 +1,14 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
-
+import { map, tap } from 'rxjs/operators';
 export default () => {
-    /** start coding */
-    const grid = document.getElementById('grid');
-    const click$ = fromEvent(grid, 'click').pipe(
-        map(val => [ 
-            Math.floor(val.offsetX/50), 
-            Math.floor(val.offsetY/50)
-        ])
-    );
-
-    const subscription = click$.subscribe(data => displayLog(data));
-
-    /** end coding */
-}
+  /** start coding */
+  const grid = document.getElementById('grid');
+  const click$ = fromEvent(grid, 'click').pipe(
+    tap(val => console.log('before: ' + val)),
+    map(val => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+    tap(val => console.log('after: ' + val))
+  );
+  const subscribe = click$.subscribe(data => displayLog(data));
+  /** end coding */
+};
