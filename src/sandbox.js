9ad3b82dd8d46dd91074106cdebb522e3bff078e
José Francisco Vargas Galán <48479726+josevargasgalan@users.noreply.github.com>
2019-06-04 09:21:25 +0200

    Lesson done

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,20 +1,25 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, takeWhile, tap } from 'rxjs/operators';
-
+import { map, takeWhile, tap, startWith, endWith } from 'rxjs/operators';
 export default () => {
-    /** start coding */
-    const grid = document.getElementById('grid');
-    const click$ = fromEvent(grid, 'click').pipe(
-        map(val => [ 
-            Math.floor(val.offsetX/50), 
-            Math.floor(val.offsetY/50)
-        ]),
-        takeWhile( ([col, row]) => col != 0 ),
-        tap(val => console.log(`cell: [${val}]`)),
-    );
+  /** start coding */
+  const grid = document.getElementById('grid');
+  /** StartWith */
+  //   const click$ = fromEvent(grid, 'click').pipe(
+  //     map(val => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+  //     takeWhile(val => val[0] !== 0),
+  //     tap(val => console.log(`cell: ${val}`)),
+  //     startWith('grid dimensions:', '10x10')
+  //   );
 
-    const subscription = click$.subscribe(data => displayLog(data));
+  /** EndWith */
+  const click$ = fromEvent(grid, 'click').pipe(
+    map(val => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+    takeWhile(val => val[0] !== 0),
+    tap(val => console.log(`cell: ${val}`)),
+    endWith('game finished', 'Bye!')
+  );
 
-    /** end coding */
-}
+  const subscribe = click$.subscribe(data => displayLog(data));
+  /** end coding */
+};
