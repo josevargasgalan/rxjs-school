99053c403468690be8e4ae4d89cf6399b573d383
José Francisco Vargas Galán <48479726+josevargasgalan@users.noreply.github.com>
2019-06-04 08:53:26 +0200

    Lesson done

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,18 +1,24 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
-
+import { map, first, take, takeWhile } from 'rxjs/operators';
 export default () => {
-    /** start coding */
-    const grid = document.getElementById('grid');
-    const click$ = fromEvent(grid, 'click').pipe(
-        map(val => [ 
-            Math.floor(val.offsetX/50), 
-            Math.floor(val.offsetY/50)
-        ])
-    );
-
-    const subscription = click$.subscribe(data => displayLog(data));
-
-    /** end coding */
-}
+  /** start coding */
+  const grid = document.getElementById('grid');
+  /** First */
+  //   const click$ = fromEvent(grid, 'click').pipe(
+  //     map(val => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+  //     first(val => val[0] > 3)
+  //   );
+  /** Take */
+  //   const click$ = fromEvent(grid, 'click').pipe(
+  //     map(val => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+  //     take(4)
+  //   );
+  /** TakeWhile */
+  const click$ = fromEvent(grid, 'click').pipe(
+    map(val => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+    takeWhile(val => val[0] > 3)
+  );
+  const subscribe = click$.subscribe(data => displayLog(data));
+  /** end coding */
+};
