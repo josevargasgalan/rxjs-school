a16d3f676a97685fae4722f51ea479a830340492
José Francisco Vargas Galán <48479726+josevargasgalan@users.noreply.github.com>
2019-06-04 09:50:43 +0200

    lesson done

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,31 +1,37 @@
 import { updateDisplay } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, pairwise } from 'rxjs/operators';
 
 export default () => {
-    /** start coding */
-    const progressBar = document.getElementById('progress-bar');
-    const docElement = document.documentElement;
-    const updateProgressBar = (percentage) => {
-        progressBar.style.width = `${percentage}%`;
-    }
+  /** start coding */
+  const progressBar = document.getElementById('progress-bar');
+  const docElement = document.documentElement;
+  const updateProgressBar = percentage => {
+    progressBar.style.width = `${percentage}%`;
+  };
 
-    //observable that returns scroll (from top) on scroll events
-    const scroll$ = fromEvent(document, 'scroll').pipe(
-        map(() => docElement.scrollTop),
-        tap(evt => console.log("[scroll]: ", evt))
-    );
+  //observable that returns scroll (from top) on scroll events
+  /** Pairwise - emite eventos en parejas de datos consecutivos, el actual y el anterior */
+  const scroll$ = fromEvent(document, 'scroll').pipe(
+    map(() => docElement.scrollTop),
+    tap(evt => console.log('[scroll]: ', evt)),
+    pairwise(),
+    tap(([previous, current]) => {
+      updateDisplay(current > previous ? 'DESC' : 'ASC');
+    }),
+    map(([previous, current]) => current)
+  );
 
-    //observable that returns the amount of page scroll progress
-    const scrollProgress$ = scroll$.pipe(
-        map(evt => {
-            const docHeight = docElement.scrollHeight - docElement.clientHeight;
-            return (evt / docHeight) * 100;
-        })
-    )
+  //observable that returns the amount of page scroll progress
+  const scrollProgress$ = scroll$.pipe(
+    map(evt => {
+      const docHeight = docElement.scrollHeight - docElement.clientHeight;
+      return (evt / docHeight) * 100;
+    })
+  );
 
-    //subscribe to scroll progress to paint a progress bar
-    const subscription = scrollProgress$.subscribe(updateProgressBar);
+  //subscribe to scroll progress to paint a progress bar
+  const subscription = scrollProgress$.subscribe(updateProgressBar);
 
-    /** end coding */
-}
+  /** end coding */
+};
