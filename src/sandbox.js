27ec47933bd7e063628fd3a17bd8b13a81a80984
José Francisco Vargas Galán <48479726+josevargasgalan@users.noreply.github.com>
2019-06-04 09:14:56 +0200

    Lesson done

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,20 +1,42 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, takeWhile, tap } from 'rxjs/operators';
-
+import { map, takeWhile, tap, reduce, scan } from 'rxjs/operators';
 export default () => {
-    /** start coding */
-    const grid = document.getElementById('grid');
-    const click$ = fromEvent(grid, 'click').pipe(
-        map(val => [ 
-            Math.floor(val.offsetX/50), 
-            Math.floor(val.offsetY/50)
-        ]),
-        takeWhile( ([col, row]) => col != 0 ),
-        tap(val => console.log(`cell: [${val}]`)),
-    );
+  /** start coding */
+  const grid = document.getElementById('grid');
+  /** Reduce */
+  //   const click$ = fromEvent(grid, 'click').pipe(
+  //     map(val => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+  //     takeWhile(val => val[0] !== 0),
+  //     tap(val => console.log(`cell: ${val}`)),
+  //     reduce(
+  //       (accumulated, current) => {
+  //         return {
+  //           clicks: accumulated.clicks + 1,
+  //           cells: [...accumulated.cells, current]
+  //         };
+  //       },
+  //       { clicks: 0, cells: [] }
+  //     )
+  //   );
+  /** Scan */
+  const click$ = fromEvent(grid, 'click').pipe(
+    map(val => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+    takeWhile(val => val[0] !== 0),
+    tap(val => console.log(`cell: ${val}`)),
+    scan(
+      (accumulated, current) => {
+        return {
+          clicks: accumulated.clicks + 1,
+          cells: [...accumulated.cells, current]
+        };
+      },
+      { clicks: 0, cells: [] }
+    )
+  );
 
-    const subscription = click$.subscribe(data => displayLog(data));
-
-    /** end coding */
-}
+  const subscribe = click$.subscribe(data =>
+    displayLog(`${data.clicks} clicks: ${JSON.stringify(data.cells)}`)
+  );
+  /** end coding */
+};
