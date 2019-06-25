448e1c00feb63359d8dbccce14be491cee15753d
José Francisco Vargas Galán <48479726+josevargasgalan@users.noreply.github.com>
2019-06-25 11:40:11 +0200

    Lesson done

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,40 +1,49 @@
 import { updateDisplay } from './utils';
-import { fromEvent } from 'rxjs';
-import { map, tap, share } from 'rxjs/operators';
+import { fromEvent, Subject, BehaviorSubject } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 export default () => {
-    /** start coding */
+  /** start coding */
+  const progressBar = document.getElementById('progress-bar');
+  const docElement = document.documentElement;
+  const updateProgressBar = percentage => {
+    progressBar.style.width = `${percentage}%`;
+  };
 
-    const progressBar = document.getElementById('progress-bar');
-    const docElement = document.documentElement;
+  //observable that returns scroll (from top) on scroll events
+  /** Share - convierte un cold observable en un hot observable consiguiendo compartir una misma instancia
+   * del observable con todas sus suscripciones
+   */
+  const scroll$ = fromEvent(document, 'scroll').pipe(
+    map(() => docElement.scrollTop),
+    tap(evt => console.log('[scroll]: ', evt))
+  );
 
-    //function to update progress bar width on view
-    const updateProgressBar = (percentage) => {
-        progressBar.style.width = `${percentage}%`;
-    }
+  //observable that returns the amount of page scroll progress
+  const scrollProgress$ = scroll$.pipe(
+    map(evt => {
+      const docHeight = docElement.scrollHeight - docElement.clientHeight;
+      return (evt / docHeight) * 100;
+    })
+  );
 
-    //observable that returns scroll (from top) on scroll events
-    const scroll$ = fromEvent(document, 'scroll').pipe(
-        map(() => docElement.scrollTop),
-        tap(evt => console.log("[scroll]: ", evt))
-    );
+  /**
+   * Subject - Por definicion es un hot observable, observer y observador.
+   */
+  // const scrollProgressHot$ = new Subject();
+  /**
+   * BehaviorSubject - Es un Subject que además tiene un estado. Este estado puede ser el valor inicial que se le da
+   * en el constructor o el ultimo valor emitido por el streaming
+   */
+  const scrollProgressHot$ = new BehaviorSubject(0);
+  scrollProgress$.subscribe(scrollProgressHot$);
+  //subscribe to scroll progress to paint a progress bar
+  const subscription = scrollProgressHot$.subscribe(updateProgressBar);
 
-    //observable that returns the amount of page scroll progress
-    const scrollProgress$ = scroll$.pipe(
-        map(evt => {
-            const docHeight = docElement.scrollHeight - docElement.clientHeight;
-            return (evt / docHeight) * 100;
-        }),
-        share()
-    )
+  const subscription2 = scrollProgressHot$.subscribe(updateProgressBar => {
+    updateDisplay(`${Math.floor(updateProgressBar)} %`);
+  });
 
-    //subscribe to scroll progress to paint a progress bar
-    const subscription = scrollProgress$.subscribe(updateProgressBar);
-
-    //subscribe to display scroll progress percentage
-    const subscription2 = scrollProgress$.subscribe(
-        val => updateDisplay(`${ Math.floor(val) } %`)
-    );
-
-    /** end coding */
-}
+  // scrollProgressHot$.next(0);
+  /** end coding */
+};
