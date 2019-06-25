4e25fbb539af9babc4586aeb1acaf28141cd285e
José Francisco Vargas Galán <48479726+josevargasgalan@users.noreply.github.com>
2019-06-25 15:52:22 +0200

    Lesson done

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,32 +1,52 @@
 import { updateDisplay, displayLog } from './utils';
 import { api } from './api';
 import { concat, fromEvent } from 'rxjs';
-import { map, endWith } from 'rxjs/operators';
+import { map, endWith, tap, mergeAll, mergeMap } from 'rxjs/operators';
 
 export default () => {
-    /** start coding */
-    
-    const button = document.getElementById('btn');
+  /** start coding */
 
-    /** get 4 consecutive comments */
-    const getComments = () =>{
-        //get observables from fake REST API.
-        const comment1$ = api.getComment(1);
-        const comment2$ = api.getComment(2);
-        const comment3$ = api.getComment(3);
-        const comment4$ = api.getComment(4);
+  const button = document.getElementById('btn');
 
-        //subscribe to all the observables to get and display comments
-        concat(comment1$, comment2$, comment3$, comment4$).pipe(
-            map(JSON.stringify),
-            endWith('--------//--------')
-        ).subscribe(data =>{
-            displayLog(data);
-        })
-    }
+  /** get 4 consecutive comments */
+  const getComments = () => {
+    //get observables from fake REST API.
+    const comment1$ = api.getComment(1);
+    const comment2$ = api.getComment(2);
+    const comment3$ = api.getComment(3);
+    const comment4$ = api.getComment(4);
 
-    /** get comments on button click */
-    fromEvent(button, 'click').subscribe(getComments);
+    //subscribe to all the observables to get and display comments
+    return concat(comment1$, comment2$, comment3$, comment4$)
+      .pipe(
+        map(JSON.stringify),
+        endWith('--------//--------')
+      )
+  };
 
-    /** end coding */
-}
+  const observable2$ = api.getComment(1).pipe(map(JSON.stringify));
+
+  /** get comments on button click */
+  /** MergeAll - se usa en observables que emiten observables (High Order Observables),
+   * se suscribe a los observables internos y emite sus eventos como si fueran eventos
+   * del HOO*/
+  // fromEvent(button, 'click')
+  //   .pipe(
+  //     map(evt => observable2$),
+  //     mergeAll(),
+  //     tap(console.log)
+  //   )
+  //   .subscribe(displayLog);
+
+  /** MergeMap - MergeMap se suscribe a los observables internos y emite sus valores a 
+   * través del observable externo o HOO
+   */
+  fromEvent(button, 'click')
+  .pipe(
+    mergeMap(evt => getComments()),
+    tap(console.log)
+  )
+  .subscribe(displayLog);
+
+  /** end coding */
+};
