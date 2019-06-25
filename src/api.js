683a2a029c00f762c97a3849bba8160463717c3e
José Francisco Vargas Galán <48479726+josevargasgalan@users.noreply.github.com>
2019-06-25 16:39:36 +0200

    Lesson done

--- src/api.js
+++ src/api.js
@@ -1,24 +1,30 @@
-import { timer } from 'rxjs';
-import { mapTo } from 'rxjs/operators';
+import { timer, throwError, of } from 'rxjs';
+import { mapTo, mergeMap } from 'rxjs/operators';
 
-export class api{
-    static getComment(id){
-        return timer(Math.random()*1000).pipe(
-            mapTo({id:id, comment:`comment number ${id}`})
-        );
-    }
-
-    static getCommentsList(page){
-        const buildCommentsList = (page) =>{
-            let comments = [];
-            const offset = (page-1)*10;
-            for(let i=offset; i < offset+10; i++){
-                comments.push({id:i, comment:`comment number ${i}`})
-            }
-            return comments;
+  /** ThrowError -  Lanza un excepciones en el flujo d eun observable */
+export class api {
+  static getComment(id) {
+    return timer(Math.random() * 1000).pipe(
+      mergeMap(evt => {
+        const isError = Math.random() > 0.6 ? true : false;
+        if (isError) {
+          return throwError(new Error('Request Timeout'));
+        } else {
+          return of({ id: id, comment: `comment number ${id}` });
         }
-        return timer(Math.random()*1000).pipe(
-            mapTo(buildCommentsList(page))
-        );
-    }    
+      })
+    );
+  }
+
+  static getCommentsList(page) {
+    const buildCommentsList = page => {
+      let comments = [];
+      const offset = (page - 1) * 10;
+      for (let i = offset; i < offset + 10; i++) {
+        comments.push({ id: i, comment: `comment number ${i}` });
+      }
+      return comments;
+    };
+    return timer(Math.random() * 1000).pipe(mapTo(buildCommentsList(page)));
+  }
 }
