74bf13f4102a595348ad7c931caa9c6d22eed243
José Francisco Vargas Galán <48479726+josevargasgalan@users.noreply.github.com>
2019-06-26 08:30:01 +0200

    Lesson done

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,33 +1,46 @@
-import { updateDisplay } from './utils';
-import { fromEvent, interval } from 'rxjs';
-import { mapTo, scan, takeWhile  } from 'rxjs/operators';
+import { updateDisplay } from "./utils";
+import { fromEvent, interval, merge, NEVER, EMPTY } from "rxjs";
+import { mapTo, scan, takeWhile, switchMap, startWith } from "rxjs/operators";
 
 export default () => {
-    /** start coding */
+  /** start coding */
 
-    /** number of seconds to init countdown */
-    const countdownSeconds = 10;
-    
-    /** access interface buttons */
-    const pauseButton = document.getElementById('pause-btn');
-    const resumeButton = document.getElementById('resume-btn');
+  /** number of seconds to init countdown */
+  const countdownSeconds = 10;
 
-    /** get comments on button click */
-    const pause$ = fromEvent(pauseButton, 'click');
-    const resume$ = fromEvent(resumeButton, 'click');
+  /** access interface buttons */
+  const pauseButton = document.getElementById("pause-btn");
+  const resumeButton = document.getElementById("resume-btn");
 
-    /** 1s negative interval */
-    const interval$ = interval(1000).pipe(mapTo(-1));
+  /** get comments on button click */
+  const pause$ = fromEvent(pauseButton, "click");
+  const resume$ = fromEvent(resumeButton, "click");
+  const isPaused$ = merge(pause$.pipe(mapTo(true)), resume$.pipe(mapTo(false)));
 
-    /** countdown timer */
-    const countdown$ = interval$.pipe(
-        scan((acc, curr) => ( curr ? curr + acc : curr ), countdownSeconds),
-        takeWhile(v => v >= 0)
-    );
+  /** 1s negative interval */
+  const interval$ = interval(1000).pipe(mapTo(-1));
 
-    /** subscribe to countdown */
-    countdown$.subscribe(updateDisplay);
+  /** Never - es una funcion que devuelve un observable vacio, no emite ningun valor, ni lanza
+   * un error ni se completa */
+  /** countdown timer */
+//   const countdown$ = isPaused$.pipe(
+//     startWith(false),
+//     switchMap(paused => (!paused ? interval$ : NEVER)),
+//     scan((acc, curr) => (curr ? curr + acc : curr), countdownSeconds),
+//     takeWhile(v => v >= 0)
+//   );
 
-    
-    /** end coding */
-}
+  /** Empty - es una funcion que devuelve un observable vacio, que tal como recibe un suscripcion
+   * se completa */
+  const countdown$ = isPaused$.pipe(
+    startWith(false),
+    switchMap(paused => (!paused ? interval$ : EMPTY)),
+    scan((acc, curr) => (curr ? curr + acc : curr), countdownSeconds),
+    takeWhile(v => v >= 0)
+  );
+
+  /** subscribe to countdown */
+  countdown$.subscribe(updateDisplay);
+
+  /** end coding */
+};
